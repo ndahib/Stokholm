c46b01b3731307b929093bc336a584f477b7c713
ndahib <ndahib@gmail.com>
2025-12-23 10:37:58 +0100

    Remove a useless comment from main

--- src/main.py
+++ src/main.py
@@ -6,14 +6,13 @@
 #    By: ndahib <ndahib@student.1337.ma>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/12/13 10:56:03 by ndahib            #+#    #+#              #
-#    Updated: 2025/12/13 10:56:04 by ndahib           ###   ########.fr        #
+#    Updated: 2025/12/23 10:37:33 by ndahib           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from manager import execute_from_command_line
 
 def main():
-    """Main function to run the web scraper."""
 
     execute_from_command_line()
 
